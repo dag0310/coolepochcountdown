889072b0d929e12d303e691c2b792d60e913bad5
Chris Rowe <mail@chrisrowe.net>
2011-08-02 20:37:35 +0100

    Gauges added

--- index.php
+++ index.php
@@ -128,6 +128,18 @@ function missedit(){
 var pageTracker=_gat._getTracker("UA-7308649-1");
 pageTracker._trackPageview();
 
+var _gauges = _gauges || [];
+(function() {
+	var t   = document.createElement('script');
+	t.type  = 'text/javascript';
+	t.async = true;
+	t.id    = 'gauges-tracker';
+	t.setAttribute('data-site-id', '4e3850e6613f5d7326000001');
+	t.src = '//secure.gaug.es/track.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(t, s);
+})();
+
 </script>
 </body>
 </html>
