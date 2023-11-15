87032488e7cbe4ba141c53b8c058c1afade0b335
Daniel Geymayer <dag0310@gmail.com>
2023-11-15 15:04:50 +0100

    Add multiple cool epoch times in the future

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
-<?php $coolepoch = !empty($_GET['t']) ? $_GET['t'] : '1234567890'; ?><!DOCTYPE html>
+<?php $coolepochtimes = [1234567890, 1700000000, 1717171717, 1777777777, 1800000000, 1818181818, 1888888888, 1900000000, 1919191919, 1999999999, 2000000000, 202020202020, 2100000000, 2121212121, 2147485547]; ?>
+<?php $coolepochtimes_in_future = []; foreach ($coolepochtimes as $cet) { if (time() > $cet) continue; $coolepochtimes_in_future[] = $cet; } ?>
+<?php $coolepoch = !empty($_GET['t']) ? $_GET['t'] : strval($coolepochtimes_in_future[0]); ?><!DOCTYPE html>
 <html>
 <head>
 	<title>Countdown (actually up) to the UNIX Epoch time being <?= $coolepoch ?>!</title>
@@ -17,14 +19,21 @@
 	<span id="timer"><script>var d = new Date();var t = d.getTime();var o = t+"";document.write(o.substring(0,10));</script></span>
 	<br>
 
-	<p class="desc">
+	<p id="desc" class="desc">
 		Only <span id="countdown"></span> until the Epoch Time is <strong><?= $coolepoch ?>!</strong> (<script>document.write(new Date(<?= $coolepoch * 1000 ?>))</script>)
 	</p>
 
-	<p class="desc2">
+	<p class="desc">
+		Here are some other cool epoch times:
+<?php foreach ($coolepochtimes_in_future as $cetif): ?>
+		<a href="?t=<?= $cetif ?>"><?= $cetif ?></a>
+<?php endforeach ?>
+	</p>
+
+	<p id="desc2" class="desc2">
 		A quick idea by <a href='http://www.chrisrowe.net'>Chris Rowe</a> follow him on Twitter <a href='http://www.twitter.com/chrisrowe'>@chrisrowe</a>
 		<br>
-		Extended by <a href="https://geymayer.com">Daniel Geymayer</a>
+		Extended beyond 1234567890 by <a href="https://geymayer.com">Daniel Geymayer</a>
 	</p>
 </div>
 
@@ -33,12 +42,11 @@
 <script>
 var wooAudio = new Audio('audio/woo.mp3');
 
-recount();
 var doparty=false;
 var finish=0;
 var appended=false;
 setInterval(recount,1000);
-
+recount();
 function recount(){
 
 	var d=new Date();
@@ -47,22 +55,28 @@ function recount(){
 	var amn=60;
 	var ahr=amn*60;
 	var ady=ahr*24;
-	var awk=ady*7;
+	var ayr=ady*365;
 	var diff=<?=$coolepoch;?>-o.substring(0,10);
 	var scs=diff;
 	var mns=(diff-(diff%amn))/amn;
 	var hrs=(diff-(diff%ahr))/ahr;
 	var dys=(diff-(diff%ady))/ady;
-	var wks=(diff-(diff%awk))/awk;
-	dys=dys%7;
+	var yrs=(diff-(diff%ayr))/ayr;
+	dys=dys%365;
 	hrs=hrs%24;
 	mns=mns%60;
 	scs=scs%60;
 
 	var thetext ="";
 
+	if (yrs>0){
+		thetext+=yrs+" year";
+		if(yrs!=1){thetext+="s";}
+		thetext+=", ";
+	}
+
 	if (dys>0){
-		thetext=dys+" day";
+		thetext+=dys+" day";
 		if(dys!=1){thetext+="s";}
 		thetext+=", ";
 	}
@@ -96,7 +110,7 @@ function recount(){
 
 	if(finish<30){
 		if(doparty==true){
-			$(".desc").text("Woohoo!!! The epoch timestamp just passed <?= $coolepoch ?>!!!");
+			$("#desc").text("Woohoo!!! The epoch timestamp just passed <?= $coolepoch ?>!!!");
 			var tweeturl="http://twitter.com/home?status=I just watched the unix epoch time reach <?= $coolepoch ?> http://coolepochcountdown.com WAHOO";
 			$("#tweet").attr("href",tweeturl);
 			setTimeout('createFirework(20,50,2,null,null,null,null,null,Math.random()>0.5,true);',(Math.floor(Math.random()*7500)));
@@ -122,13 +136,13 @@ function partytime(){
 	doparty=true;
 	document.title="HAPPY <?= $coolepoch ?>!!!";
 	wooAudio.play();
-	$(".desc").text("Woohoo!!! The epoch timestamp just passed <?= $coolepoch ?>!!!");
+	$("#desc").text("Woohoo!!! The epoch timestamp just passed <?= $coolepoch ?>!!!");
 }
 
 function missedit(){
 	if (appended == false){
-		$(".desc2").append("&nbsp;&bull;&nbsp;<span class='fun'>Relive the moment</span>");
-		$(".desc").text("You missed <?=$coolepoch;?>, but fear not - the next cool epoch time will come soon enough :)");
+		$("#desc2").append("&nbsp;&bull;&nbsp;<span class='fun'>Relive the moment</span>");
+		$("#desc").text("You missed <?=$coolepoch;?>, but fear not - the next cool epoch time will come soon enough :)");
 		var tweeturl="http://twitter.com/home?status=I just watched the unix epoch time reach <?= $coolepoch ?> http://coolepochcountdown.com WAHOO";
 		$("#tweet").attr("href",tweeturl);
 		appended=true;
